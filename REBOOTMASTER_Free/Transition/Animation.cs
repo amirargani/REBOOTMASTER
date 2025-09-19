6a4fe26cfa0b72acaa0f96ec35977344446247a0
Amir Argani <amirargani@yahoo.com>
2025-09-19 14:49:39 +0200

    Add project files.

---
+++ REBOOTMASTER_Free/Transition/Animation.cs
@@ -0,0 +1,36 @@
+﻿namespace REBOOTMASTER_Free.Transition
+{
+    internal class Animation
+    {
+        public int Duration { get; set; }
+        public AnimationFlags Flags { get; set; }
+        public string? Name { get; set; }
+
+        public event Action<Animation>? AnimationStarted;
+        public event Action<Animation>? AnimationEnded;
+
+        /// <summary>
+        /// Triggers the AnimationStarted event.
+        /// </summary>
+        public void OnStarted() => AnimationStarted?.Invoke(this);
+
+        /// <summary>
+        /// Triggers the AnimationEnded event.
+        /// </summary>
+        public void OnEnded() => AnimationEnded?.Invoke(this);
+
+        [Flags]
+        public enum AnimationFlags
+        {
+            Show = 0X20000,
+            Hide = 0X10000,
+            Slide = 0X40000,
+            Fade = 0x80000,
+            Center = 0x10,
+            RightToLeft = 0x2,
+            LeftToRight = 0X1,
+            TopToBottom = 0x4,
+            BottomToTop = 0x8
+        }
+    }
+}
---
+++ REBOOTMASTER_Free/UserControls/US_Dashboard.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace REBOOTMASTER_Free.UserControls
+{
+    partial class US_Dashboard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(US_Dashboard));
+            pictureBox_Arrow = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox_Arrow).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox_Arrow
+            // 
+            pictureBox_Arrow.BackgroundImageLayout = ImageLayout.None;
+            pictureBox_Arrow.Image = (Image)resources.GetObject("pictureBox_Arrow.Image");
+            pictureBox_Arrow.Location = new Point(452, 223);
+            pictureBox_Arrow.Name = "pictureBox_Arrow";
+            pictureBox_Arrow.Size = new Size(724, 497);
+            pictureBox_Arrow.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox_Arrow.TabIndex = 30;
+            pictureBox_Arrow.TabStop = false;
+            // 
+            // US_Settings
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(0, 8, 25);
+            Controls.Add(pictureBox_Arrow);
+            Name = "US_Settings";
+            Size = new Size(800, 450);
+            ((System.ComponentModel.ISupportInitialize)pictureBox_Arrow).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private PictureBox pictureBox_Arrow;
+    }
+}
